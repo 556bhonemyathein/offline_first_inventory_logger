54a48bf3f5643b685720fec68e59efd67712dc15
mgbhone556 <mgbhone556@gmail.com>
2026-05-27 08:15:39 +0630

    update api_service.dart
    inputting api_service with real data and fake data  fortesting for network issue

--- lib/controller/services/api_service.dart
+++ lib/controller/services/api_service.dart
@@ -1,47 +1,5 @@
-// import 'package:dio/dio.dart';
-// import 'package:logger/logger.dart';
-// import '../../model/supplier_model.dart';
-
-// class ApiService {
-//   final logger = Logger();
-
-//   final Dio dio = Dio(
-//     BaseOptions(
-//       connectTimeout: const Duration(seconds: 10),
-//       receiveTimeout: const Duration(seconds: 10),
-//     ),
-//   );
-
-//   Future<List<SupplierModel>> fetchSuppliers() async {
-//     try {
-//       logger.i("START API");
-
-//       final response = await dio.get(
-//         'https://jsonplaceholder.typicode.com/users',
-//       );
-
-//       logger.i("STATUS CODE: ${response.statusCode}");
-
-//       logger.d(response.data);
-
-//       final data = response.data as List;
-
-//       final suppliers = data.map((e) => SupplierModel.fromJson(e)).toList();
-
-//       logger.i("SUPPLIERS COUNT: ${suppliers.length}");
-
-//       return suppliers;
-//     } catch (e) {
-//       logger.e("API ERROR: $e");
-
-//       rethrow;
-//     }
-//   }
-// }
-
 import 'package:dio/dio.dart';
 import 'package:logger/logger.dart';
-
 import '../../model/supplier_model.dart';
 
 class ApiService {
@@ -53,15 +11,57 @@ class ApiService {
       receiveTimeout: const Duration(seconds: 10),
     ),
   );
-  Future<List<SupplierModel>> fetchSuppliers() async {
-    await Future.delayed(const Duration(seconds: 1));
 
-    return [
-      SupplierModel(id: 1, name: "Leanne Graham"),
-      SupplierModel(id: 2, name: "Ervin Howell"),
-      SupplierModel(id: 3, name: "Clementine Bauch"),
-      SupplierModel(id: 4, name: "Patricia Lebsack"),
-      SupplierModel(id: 5, name: "Chelsey Dietrich"),
-    ];
+  Future<List<SupplierModel>> fetchSuppliers() async {
+    try {
+      logger.i("START API");
+
+      final response = await dio.get(
+        'https://jsonplaceholder.typicode.com/users',
+      );
+
+      logger.i("STATUS CODE: ${response.statusCode}");
+
+      logger.d(response.data);
+
+      final data = response.data as List;
+
+      final suppliers = data.map((e) => SupplierModel.fromJson(e)).toList();
+
+      logger.i("SUPPLIERS COUNT: ${suppliers.length}");
+
+      return suppliers;
+    } catch (e) {
+      logger.e("API ERROR: $e");
+
+      rethrow;
+    }
   }
 }
+
+// import 'package:dio/dio.dart';
+// import 'package:logger/logger.dart';
+
+// import '../../model/supplier_model.dart';
+
+// class ApiService {
+//   final logger = Logger();
+
+//   final Dio dio = Dio(
+//     BaseOptions(
+//       connectTimeout: const Duration(seconds: 10),
+//       receiveTimeout: const Duration(seconds: 10),
+//     ),
+//   );
+//   Future<List<SupplierModel>> fetchSuppliers() async {
+//     await Future.delayed(const Duration(seconds: 1));
+
+//     return [
+//       SupplierModel(id: 1, name: "Leanne Graham"),
+//       SupplierModel(id: 2, name: "Ervin Howell"),
+//       SupplierModel(id: 3, name: "Clementine Bauch"),
+//       SupplierModel(id: 4, name: "Patricia Lebsack"),
+//       SupplierModel(id: 5, name: "Chelsey Dietrich"),
+//     ];
+//   }
+// }
